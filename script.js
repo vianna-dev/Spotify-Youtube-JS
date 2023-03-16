c129e92e0d5a514abcd2c1f815451f2be4f8f360
vianna-dev <gabrielvianna.dev@Outloook.com>
2023-03-16 07:46:03 -0300

    spotify-youtube-JS

---
+++ script.js
@@ -0,0 +1,80 @@
+let musicas = [
+    {titulo:'Eletronica', artista:'Gabriel Vianna', src:'musicas/Gemini - half.cool.mp3', img:'imagens/eletronica.jpg'},
+    {titulo:'Jazz classico', artista:'Jazz envolvente', src:'musicas/Getz Me to Brazil - Doug Maxwell_Media Right Productions.mp3', img:'imagens/jazz.jpg'},
+    {titulo:'Reggae raiz', artista:'Bob Marley', src:'musicas/Sunny Over the Horizon - RAGE.mp3', img:'imagens/reggae.jpg'}
+];
+
+let musica = document.querySelector('audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
+
+// Eventos
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra);
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2) {
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
+
+
+// Funções
+function renderizarMusica(index) {
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
+function tocarMusica() {
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+function pausarMusica() {
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector('progress')
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos) {
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10) {
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos + ':' + campoSegundos;
+}
+
